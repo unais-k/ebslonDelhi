185aa939b91c6a838e7ba09a4af29669bc97d662
unais-k <unaissafa008@gmail.com>
2023-10-07 11:10:05 +0530

    finishing touch

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -4,16 +4,27 @@ import Navbar from "./Navbar";
 import Post from "./Post";
 import { GetAllBlog } from "../Api/Services/blog";
 import Loader from "./Loader";
+import { useSelector } from "react-redux";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
     const [blog, setBlog] = useState();
     const [loader, setLoader] = useState(false);
+    const token = useSelector((state) => state.userLogin.token);
+    const navigate = useNavigate();
+
     const getAllBlogAPICall = async () => {
         setLoader(true);
         const response = await GetAllBlog();
         if (response.status === 202) {
             const data = response.data;
-            setBlog(data.result);
+            if (token) {
+                setBlog(data.result);
+            } else {
+                const firstPart = data.result.slice(0, 3);
+                setBlog(firstPart);
+            }
             setLoader(false);
         } else {
             console.log("No blog");
@@ -33,6 +44,13 @@ function Home() {
                 {loader && <Loader />}
                 <div className={`${styles.boxWidth}`}>
                     {blog?.length > 0 && blog.map((post, index) => <Post key={index} {...post} />)}
+                    {token ? (
+                        ""
+                    ) : (
+                        <div className="my-5 flex w-fit" onClick={() => navigate("/login")}>
+                            <Button title={"For more blogs Login"} />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
